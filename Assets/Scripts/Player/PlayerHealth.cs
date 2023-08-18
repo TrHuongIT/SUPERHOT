c41b3a0a97a8d1000f241bfbe3fef1c2434d50d7
TrHuongIT <tramhuongptit@gmail.com>
2023-08-18 20:44:00 +0700

    Effect Damage

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    
     private float health;
     private float lerpTime;
+    [Header("Health Bar")]
     public float maxHealth = 100f;
     public float chipSpeed = 2f;
 
     public Image frontHB;
     public Image backHB;
+    public TextMeshProUGUI heal;
+
+    [Header("Damage Overlay")]
+    public Image damgeOverlay;
+    public float duration;
+    public float fadeSpeed;
+
+    private float durationTimer;
     void Start()
     {
         health = maxHealth;
+        damgeOverlay.color = new Color(damgeOverlay.color.r, damgeOverlay.color.g, damgeOverlay.color.b, 0);
     }
 
     // Update is called once per frame
@@ -22,6 +34,19 @@ public class PlayerHealth : MonoBehaviour
     {
         health = Mathf.Clamp(health, 0, maxHealth);
         UpdateHealthUI();
+        heal.SetText(health.ToString() + "/100");
+
+        if(damgeOverlay.color.a > 0)
+        {
+            if (health < 30) return;
+            durationTimer += Time.deltaTime;
+            if(durationTimer > duration)
+            {
+                float tempAlpha = damgeOverlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                damgeOverlay.color = new Color(damgeOverlay.color.r, damgeOverlay.color.g, damgeOverlay.color.b, tempAlpha);
+            }
+        }
         if (Input.GetKeyDown(KeyCode.L))
         {
             TakeDame(Random.Range(5, 10));
@@ -31,6 +56,8 @@ public class PlayerHealth : MonoBehaviour
         {
             RestoreHealth(Random.Range(20, 50));
         }
+
+        
     }
 
     public void UpdateHealthUI()
@@ -70,6 +97,8 @@ public class PlayerHealth : MonoBehaviour
     {
         health -= damage;
         lerpTime = 0f;
+        durationTimer = 0f;
+        damgeOverlay.color = new Color(damgeOverlay.color.r, damgeOverlay.color.g, damgeOverlay.color.b, 1);
     }
 
     public void RestoreHealth(float crossFood)
